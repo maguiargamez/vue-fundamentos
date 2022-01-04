15c3c82252147874758c6d453a78d8b275bc0d36
[Manuel] <[manuelaguiargamez@gmail.com]>
2022-01-04 16:03:59 -0600

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Counter:</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ counter*counter }}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            counter: 5
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
