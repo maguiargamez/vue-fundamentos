177e30e0fe86c23a25d2d836bb55d8def1fddfe1
[Manuel] <[manuelaguiargamez@gmail.com]>
2022-01-05 16:15:23 -0600

    2do commit

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,11 @@
 <template>
   <h2>Counter:</h2>
-  <p>{{ counter }} <sup>2</sup> = {{ counter*counter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+
+  <div>
+      <button @click=increaseCounter >1+</button>
+      <button @click=decreaseCounter>1-</button>
+  </div>
 </template>
 
 <script>
@@ -9,10 +14,38 @@ export default {
         return {
             counter: 5
         }
+    },
+    methods: {
+        getSquareValue(){
+            console.log('getSquareValue')
+            return this.counter * this.counter
+        },
+        increaseCounter(){
+            this.counter++
+        },
+        decreaseCounter(){
+            this.counter--
+        }
+    },
+    computed: {
+        squareCounter(){
+            console.log('computed counter')
+            return this.counter * this.counter
+        }
     }
 }
 </script>
 
 <style>
+button{
+    background-color: #64b687;
+    border-radius: 5px;
+    border: 1px solid  white;
+    color: white;
+    cursor: pointer;
+    margin: 0, 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
 
+}
 </style>
